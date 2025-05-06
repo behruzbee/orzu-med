b6c31efceae0c64291258d29e731f3c360414087
Behruz <behruzbahtiyorov10@gmail.com>
2025-05-07 03:23:03 +0500

    main2

--- src/pages/layout/protector/index.tsx
+++ src/pages/layout/protector/index.tsx
@@ -6,7 +6,7 @@ import { useGetMeQuery } from 'entities/auth';
 const ProtectorLayout = () => {
         const { data: me, isFetched } = useGetMeQuery()
 
-        if (!me && !isFetched) {
+        if (!me?.id && isFetched) {
                 return <Navigate to="/login" />
         }
 
