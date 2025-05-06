95df2ef676895459e014bcd50b15ddbc10fb5f0d
Behruz <behruzbahtiyorov10@gmail.com>
2025-05-06 17:02:01 +0500

    main

--- src/shared/api/base-api.ts
+++ src/shared/api/base-api.ts
@@ -4,7 +4,7 @@ import { TOKEN } from "shared/constants/env"
 
 
 const clientApi = axios.create({
-	baseURL: import.meta.env.VITE_APP_BASE_URL,
+	baseURL: "https:/orzumedbackend-production.up.railway.app",
 	withCredentials: true
 })
 
