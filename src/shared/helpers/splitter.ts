ba2f7c79c096b86cd07c234c17829cf1ae2c66f6
Behruz <behruzbahtiyorov10@gmail.com>
2025-05-08 11:44:38 +0500

    main

--- src/shared/helpers/splitter.ts
+++ src/shared/helpers/splitter.ts
@@ -1,46 +1,33 @@
-/* @ts-ignore */
+// @ts-ignore
 export function collectGroupedByWeek(markets) {
   const result = []
-
   const numMarkets = markets.length
-  const numRows = markets[0][0].length
-  const numWeeks = markets[0][0][0].length - 2
+  const numTables = markets[0].length
 
-  for (let weekIndex = 0; weekIndex < numWeeks; weekIndex++) {
-    const weekData = []
+  for (let tableIndex = 0; tableIndex < numTables; tableIndex++) {
+    const numRows = markets[0][tableIndex].length
+    const numWeeks = markets[0][tableIndex][0].length - 2 // минус первые 2 колонки
 
-    for (let rowIndex = 0; rowIndex < numRows; rowIndex++) {
-      const row = []
+    for (let weekIndex = 0; weekIndex < numWeeks; weekIndex++) {
+      const weekData = []
 
-      const code = markets[0][0][rowIndex][0]
-      const name = markets[0][0][rowIndex][1]
-      row.push(code, name)
+      for (let rowIndex = 0; rowIndex < numRows; rowIndex++) {
+        const row = []
 
-      for (let marketIndex = 0; marketIndex < numMarkets; marketIndex++) {
-        const value = markets[marketIndex][0][rowIndex][weekIndex + 2]
-        row.push(value)
+        const code = markets[0][tableIndex][rowIndex][0]
+        const name = markets[0][tableIndex][rowIndex][1]
+        row.push(code, name)
+
+        for (let marketIndex = 0; marketIndex < numMarkets; marketIndex++) {
+          const marketTable = markets[marketIndex][tableIndex]
+          const value = marketTable[rowIndex][weekIndex + 2] ?? null
+          row.push(value)
+        }
+
+        weekData.push(row)
       }
 
-      weekData.push(row)
-    }
-
-    result.push(weekData)
-  }
-
-  console.log(result);
-  return result
-}
-
-export function bb12(tables: number[][][], week: number) {
-  const result = []
-
-  if (tables) {
-    for (let i = 0; i < 27; i++) {
-      const table = tables[i]
-      if (table) {
-        const res = table[week - 1]
-        result.push(res)
-      }
+      result.push(weekData)
     }
   }
 
